3e2061c95f9bd9bf92170c1327b7738d0662948e
mr-fuller <mrfuller460@gmail.com>
2018-02-20 14:02:31 -0500

    fix where clause

--- off_peak_dwdh_2011.sql
+++ off_peak_dwdh_2011.sql
@@ -24,7 +24,7 @@ off_peak_dwdh_2011 as
 tmc_code,
 round(sum(delay_hours*miles),2) as off_peak_dwdh_2011
 from determine_delay_hours
-where date_part('hour', measurement_tstamp)  < 6 or date_part('hour', measurement_tstamp)  > 17 and date_part('year', measurement_tstamp) = 2011
+where (date_part('hour', measurement_tstamp)  < 6 or date_part('hour', measurement_tstamp)  > 17) and date_part('year', measurement_tstamp) = 2011
 group by tmc_code
 )
 
