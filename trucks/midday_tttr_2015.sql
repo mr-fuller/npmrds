544cb207dc0a21b0cbe205874dd847ee5b785ac7
mr-fuller <mrfuller460@gmail.com>
2018-08-14 12:22:21 -0400

    upload tttr files to separate folder

---
+++ trucks/midday_tttr_2015.sql
@@ -0,0 +1,45 @@
+--drop table congestion_lottr;
+--create table congestion_lottr as
+alter table tttr
+add column if not exists tttr_midd_2015 numeric,
+add column if not exists ttt_midd50pct_2015 numeric,
+add column if not exists ttt_midd95pct_2015 numeric;
+
+with 
+joined as(
+	
+select i.*,
+g.miles,
+g.geom
+from npmrds2012to2016truck10min_no_null as i
+full join tmacog_tmcs as g
+on g.tmc = i.tmc_code
+),
+
+apl as (
+select
+tmc_code,
+--geom,
+percentile_disc(0.95) within group (order by travel_time_seconds) as ttt_midd95pct_2015,
+percentile_disc(0.5) within group (order by travel_time_seconds) as ttt_midd50pct_2015,
+case when(percentile_disc(0.5) within group (order by travel_time_seconds) = 0)
+	then null
+	else round(cast(percentile_disc(0.95) within group (order by travel_time_seconds)/percentile_disc(0.5) within group (order by travel_time_seconds) as numeric),2) 
+	end as tttr
+
+from joined
+where date_part('year',measurement_tstamp) = 2015 and  
+--tmc_code = '108+12989' and 
+--Mon-Fri
+(extract(dow from measurement_tstamp )>0 and extract(dow from measurement_tstamp ) < 6) and 
+	--AM Peak
+	(date_part('hour',measurement_tstamp) > 8 and date_part('hour',measurement_tstamp) < 14)
+	group by tmc_code, geom
+)
+
+update tttr
+set tttr_midd_2015 = apl.tttr,
+ttt_midd50pct_2015 = apl.ttt_midd50pct_2015,
+ttt_midd95pct_2015 = apl.ttt_midd95pct_2015
+from apl
+where tttr.tmc_code = apl.tmc_code
---
+++ trucks/ovn_tttr_2014.sql
@@ -0,0 +1,45 @@
+--drop table tttr;
+--create table tttr as
+alter table tttr
+add column if not exists tttr_ovn_2014 numeric,
+add column if not exists ttt_ovn50pct_2014 numeric,
+add column if not exists ttt_ovn95pct_2014 numeric;
+
+with 
+joined as(
+	
+select i.*,
+g.miles,
+g.geom
+from npmrds2012to2016truck10min_no_null as i
+full join tmacog_tmcs as g
+on g.tmc = i.tmc_code
+),
+
+apl as (
+select
+tmc_code,
+--geom,
+percentile_disc(0.95) within group (order by travel_time_seconds) as ttt_ovn95pct_2014,
+percentile_disc(0.5) within group (order by travel_time_seconds) as ttt_ovn50pct_2014,
+case when(percentile_disc(0.5) within group (order by travel_time_seconds) = 0)
+	then null
+	else round(cast(percentile_disc(0.95) within group (order by travel_time_seconds)/percentile_disc(0.5) within group (order by travel_time_seconds) as numeric),2) 
+	end as tttr
+
+from joined
+where date_part('year',measurement_tstamp) = 2014 and  
+--tmc_code = '108+12989' and 
+
+
+	--overnight is 8 PM to 6AM every day of the week
+	(date_part('hour', measurement_tstamp)  < 6 or date_part('hour', measurement_tstamp)  > 19 )
+	group by tmc_code, geom
+)
+
+update tttr
+set tttr_ovn_2014 = apl.tttr,
+ttt_ovn50pct_2014 = apl.ttt_ovn50pct_2014,
+ttt_ovn95pct_2014 = apl.ttt_ovn95pct_2014
+from apl
+where tttr.tmc_code = apl.tmc_code
