b443eb3414f34d0547121d4d1544633244434e5e
mr-fuller <mrfuller460@gmail.com>
2018-08-15 16:53:00 -0400

    upload lottr files

---
+++ passengers/am_peak_lottr_2017.sql
@@ -0,0 +1,30 @@
+drop table congestion_lottr;
+create table congestion_lottr as
+
+with 
+joined as(
+	
+select i.*,
+g.miles,
+g.geom
+from inrix2017data as i
+full join tmacog_tmcs as g
+on g.tmc = i.tmc_code
+)
+
+
+select
+tmc_code,
+geom,
+percentile_disc(0.8) within group (order by travel_time_minutes) as percentile_eighty_2017,
+percentile_disc(0.5) within group (order by travel_time_minutes) as percentile_fifty_2017,
+round(cast(percentile_disc(0.8) within group (order by travel_time_minutes)/percentile_disc(0.5) within group (order by travel_time_minutes) as numeric),2) as am_peak_lottr_2017
+
+from joined
+where 
+--tmc_code = '108+12989' and 
+--Mon-Fri
+(extract(dow from measurement_tstamp )>0 and extract(dow from measurement_tstamp ) < 6) and 
+	--AM Peak
+	(date_part('hour',measurement_tstamp) > 5 and date_part('hour',measurement_tstamp) < 9)
+	group by tmc_code, geom;
---
+++ passengers/pm_peak_lottr_2015.sql
@@ -0,0 +1,41 @@
+--drop table congestion_lottr;
+--create table congestion_lottr as
+alter table congestion_lottr
+add column if not exists pm_peak_lottr_2015 numeric;
+
+with 
+joined as(
+	
+select i.*,
+g.miles,
+g.geom
+from inrix14to16data as i
+full join tmacog_tmcs as g
+on g.tmc = i.tmc_code
+),
+
+apl as (
+select
+tmc_code,
+--geom,
+--percentile_disc(0.8) within group (order by travel_time_minutes) as percentile_eighty_2016,
+--percentile_disc(0.5) within group (order by travel_time_minutes) as percentile_fifty_2016,
+case when(percentile_disc(0.5) within group (order by travel_time_minutes) = 0)
+	then null
+	else round(cast(percentile_disc(0.8) within group (order by travel_time_minutes)/percentile_disc(0.5) within group (order by travel_time_minutes) as numeric),2) 
+	end as lottr
+
+from joined
+where date_part('year',measurement_tstamp) = 2015 and  
+--tmc_code = '108+12989' and 
+--Mon-Fri
+(extract(dow from measurement_tstamp )>0 and extract(dow from measurement_tstamp ) < 6) and 
+	--PM Peak
+	(date_part('hour',measurement_tstamp) > 13 and date_part('hour',measurement_tstamp) < 18)
+	group by tmc_code, geom
+)
+
+update congestion_lottr
+set pm_peak_lottr_2015 = apl.lottr
+from apl
+where congestion_lottr.tmc_code = apl.tmc_code
