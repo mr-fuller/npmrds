89934f4f61393df5b8d6107b65972d185d9cb03f
mr-fuller <mrfuller460@gmail.com>
2018-08-15 16:50:21 -0400

    upload dwdh batch query file

---
+++ dwdh/all_dwdh_queries.sql
@@ -0,0 +1,55 @@
+--2017
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/am_peak_dwdh_2017.sql'
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/midday_dwdh_2017.sql'
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/pm_peak_dwdh_2017.sql'
+\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/off_peak_dwdh_2017.sql'
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/total_dwdh_2017.sql'
+
+--2016
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/am_peak_dwdh_2016.sql'
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/midday_dwdh_2016.sql'
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/pm_peak_dwdh_2016.sql'
+\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/off_peak_dwdh_2016.sql'
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/total_dwdh_2016.sql'
+
+--2015
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/am_peak_dwdh_2015.sql'
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/midday_dwdh_2015.sql'
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/pm_peak_dwdh_2015.sql'
+\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/off_peak_dwdh_2015.sql'
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/total_dwdh_2015.sql'
+
+--2014
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/am_peak_dwdh_2014.sql'
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/midday_dwdh_2014.sql'
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/pm_peak_dwdh_2014.sql'
+\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/off_peak_dwdh_2014.sql'
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/total_dwdh_2014.sql'
+
+--2013
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/am_peak_dwdh_2013.sql'
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/midday_dwdh_2013.sql'
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/pm_peak_dwdh_2013.sql'
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/off_peak_dwdh_2013.sql'
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/total_dwdh_2013.sql'
+
+--2012
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/am_peak_dwdh_2012.sql'
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/midday_dwdh_2012.sql'
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/pm_peak_dwdh_2012.sql'
+\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/off_peak_dwdh_2012.sql'
+--\i 'C:/Users/fullerm/OneDrive - TMACOG/Postgres/CMP/SQL/total_dwdh_2012.sql'
+
+--2011
+--\i 'C:/Users/fullerm/Documents/NPMRDS/cmp2017/am_peak_dwdh_2011.sql'
+--\i 'C:/Users/fullerm/Documents/NPMRDS/cmp2017/midday_dwdh_2011.sql'
+--\i 'C:/Users/fullerm/Documents/NPMRDS/cmp2017/pm_peak_dwdh_2011.sql'
+--\i 'C:/Users/fullerm/Documents/NPMRDS/cmp2017/off_peak_dwdh_2011.sql'
+--\i 'C:/Users/fullerm/Documents/NPMRDS/cmp2017/total_dwdh_2011.sql'
+
+--2010
+--\i 'C:/Users/fullerm/Documents/NPMRDS/cmp2017/am_peak_dwdh_2010.sql'
+--\i 'C:/Users/fullerm/Documents/NPMRDS/cmp2017/midday_dwdh_2010.sql'
+--\i 'C:/Users/fullerm/Documents/NPMRDS/cmp2017/pm_peak_dwdh_2010.sql'
+--\i 'C:/Users/fullerm/Documents/NPMRDS/cmp2017/off_peak_dwdh_2010.sql'
+--\i 'C:/Users/fullerm/Documents/NPMRDS/cmp2017/total_dwdh_2010.sql'
