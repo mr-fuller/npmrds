f4ca91f1644f44fe20f574f1d91f76ecaca63c03
Mike Fuller <mrfuller460@gmail.com>
2018-08-21 16:00:15 -0400

    update off peak to weekend for all years

---
+++ passengers/we_lottr_2017.sql
@@ -0,0 +1,42 @@
+--drop table congestion_lottr;
+--create table congestion_lottr as
+alter table congestion_lottr
+add column if not exists off_peak_lottr_2017 numeric;
+
+with 
+joined as(
+	
+select i.*,
+g.miles,
+g.geom
+from inrix2017data as i
+full join tmacog_tmcs as g
+on g.tmc = i.tmc_code
+),
+
+apl as (
+select
+tmc_code,
+--geom,
+--percentile_disc(0.8) within group (order by travel_time_minutes) as percentile_eighty_2016,
+--percentile_disc(0.5) within group (order by travel_time_minutes) as percentile_fifty_2016,
+case when(percentile_disc(0.5) within group (order by travel_time_minutes) = 0)
+	then null
+	else round(cast(percentile_disc(0.8) within group (order by travel_time_minutes)/percentile_disc(0.5) within group (order by travel_time_minutes) as numeric),2) 
+	end as lottr
+
+from joined
+where date_part('year',measurement_tstamp) = 2017 and  
+--tmc_code = '108+12989' and 
+--Mon-Fri
+((extract(dow from measurement_tstamp )>0 and extract(dow from measurement_tstamp ) < 6) and 
+	--off Peak
+	(date_part('hour', measurement_tstamp)  < 6 or date_part('hour', measurement_tstamp)  > 17 )) or
+	(extract(dow from measurement_tstamp) = 0 or extract(dow from measurement_tstamp) = 6)
+	group by tmc_code, geom
+)
+
+update congestion_lottr
+set off_peak_lottr_2017 = apl.lottr
+from apl
+where congestion_lottr.tmc_code = apl.tmc_code
