54b312ae01135bcbb3ceeb69058b52514359714a
Mike Fuller <mrfuller460@gmail.com>
2018-08-22 11:17:38 -0400

    rename and update queries for am peak truck dwdh for all years

---
+++ truck_dwdh/amp_truck_dwdh_2014.sql
@@ -0,0 +1,42 @@
+--drop table congestion_locations;
+--create table congestion_locations as
+alter table truck_dwdh
+add column if not exists amp_truck_dwdh_2014 numeric;
+
+with determine_delay_hours as(
+select i.*,
+case when (i.tmc_code = t.tmc)
+	then t.miles
+	else o.miles
+	end as miles,
+o.aadt,
+
+case when (i.reference_speed*0.75 > i.speed)
+  THEN (1/6)
+  ELSE 0
+end as delay_hours,
+o.geom
+from tmacog_tmcs as o
+full outer join npmrds2012to2016truck10min_no_null as i
+on o.tmc = i.tmc_code
+full join tmc_identification as t on t.tmc = i.tmc_code
+--where date_part('year', i.measurement_tstamp)  =  2016 --and date_part('hour', measurement_tstamp)  <= 14
+),
+amp_truck_dwdh_2014 as
+(select
+tmc_code,
+round(sum(delay_hours*miles),2) as amp_truck_dwdh_2014
+from determine_delay_hours
+where (date_part('hour', measurement_tstamp)  > 5 and date_part('hour', measurement_tstamp)  < 9) and
+date_part('year', measurement_tstamp) = 2014 and
+(extract(dow from measurement_tstamp )>0 and extract(dow from measurement_tstamp ) < 6)
+group by tmc_code
+)
+
+--for am Peak 6-9AM
+--am_peak as
+--(
+update truck_dwdh
+set amp_truck_dwdh_2014 = amp_truck_dwdh_2014.amp_truck_dwdh_2014
+from amp_truck_dwdh_2014
+where truck_dwdh.tmc_code = amp_truck_dwdh_2014.tmc_code ;--and (date_part('hour', measurement_tstamp)  > 8 and date_part('hour', measurement_tstamp)  < 14);
--- truck_dwdh/amp_truck_dwdh_2017.sql
+++ truck_dwdh/amp_truck_dwdh_2017.sql
@@ -1,5 +1,5 @@
-drop table if exists congestion_locations;
-create table congestion_locations as
+drop table if exists truck_dwdh;
+create table truck_dwdh as
 
 with determine_delay_hours as(
 select i.*,
@@ -17,14 +17,14 @@ o.aadt,
 --60*miles/speed as travel_time_calc,
 --60*miles/reference_speed as ref_travel_time,
 case when (i.reference_speed*0.75 > i.speed)
-  THEN 1 
+  THEN (1/6)
   ELSE 0
 end as delay_hours
 
-from npmrds2012to2017data as i
-full outer join tmacog_tmcs as o on o.tmc = i.tmc_code 
+from npmrds2017truck10min_no_null as i
+full outer join tmacog_tmcs as o on o.tmc = i.tmc_code
 full join tmc_identification as t on t.tmc = i.tmc_code
-	
+
 --where i.cvalue > 10
 --where date_part('hour', measurement_tstamp)  =  --and date_part('hour', measurement_tstamp)  <= 14
 )
@@ -33,9 +33,9 @@ full join tmc_identification as t on t.tmc = i.tmc_code
 --for am Peak 6-9AM
 --am_peak as
 --(
-select a.tmc_code, 
+select a.tmc_code,
 a.geom,
---date_part('year', measurement_tstamp), 
+--date_part('year', measurement_tstamp),
 a.miles,
 a.road,
 a.direction,
@@ -43,10 +43,10 @@ a.intersection,
 --a.miles2,
 --avg_dwdh.avg_dwdh,
 --case when (a.geom is null)
---then round(sum(a.delay_hours*a.miles2),2) 
-round(sum(a.delay_hours*a.miles),2) as am_peak_dwdh_2017
+--then round(sum(a.delay_hours*a.miles2),2)
+round(sum(a.delay_hours*a.miles),2) as amp_truck_dwdh_2017
 
- 
+
 from determine_delay_hours a --,avg_dwdh
 --join determine_delay_hours b
 --on a.tmc_code = b.tmc_code
@@ -55,4 +55,3 @@ where (date_part('hour', a.measurement_tstamp)  > 5 and date_part('hour', a.meas
  date_part('year', measurement_tstamp) = 2017
 --and date_part('hour', b.measurement_tstamp)  > 8 and date_part('hour', b.measurement_tstamp)  < 14 --note hour 8 means 8:00-8:59 is what I'm assuming
 group by a.tmc_code, a.geom, a.miles, a.road, a.direction, a.intersection;
-	
