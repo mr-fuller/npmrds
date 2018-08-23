72f74a8f8ab64f29d00a069a19d5b47afcc9d629
Mike Fuller <mrfuller460@gmail.com>
2018-08-23 16:59:56 -0400

    fix queries for total truck dwdh

--- truck_dwdh/total_truck_dwdh_2012.sql
+++ truck_dwdh/total_truck_dwdh_2012.sql
@@ -1,7 +1,7 @@
---drop table congestion_locations;
---create table congestion_locations as
-alter table congestion_locations
-add column if not exists total_dwdh_2012 numeric;
+--drop table truck_dwdh;
+--create table truck_dwdh as
+alter table truck_dwdh
+add column if not exists total_truck_dwdh_2012 numeric;
 
 with determine_delay_hours as(
 select i.*,
@@ -12,21 +12,21 @@ case when (i.tmc_code = t.tmc)
 o.aadt,
 
 case when (i.reference_speed*0.75 > i.speed)
-  THEN 1 
+  THEN (1/6)
   ELSE 0
 end as delay_hours,
 o.geom
 from tmacog_tmcs as o
 full outer join npmrds2012to2017data as i
-on o.tmc = i.tmc_code 
+on o.tmc = i.tmc_code
 full join tmc_identification as t on t.tmc = i.tmc_code
 --where i.cvalue > 10
 --where date_part('hour', measurement_tstamp)  =  --and date_part('hour', measurement_tstamp)  <= 14
 ),
-total_dwdh_2012 as
+total_truck_dwdh_2012 as
 (select
 tmc_code,
-round(sum(delay_hours*miles),2) as total_dwdh_2012
+round(sum(delay_hours*miles),2) as total_truck_dwdh_2012
 from determine_delay_hours
 where date_part('year', measurement_tstamp) = 2012
 group by tmc_code
@@ -34,13 +34,7 @@ group by tmc_code
 
 --for all hours
 
-update congestion_locations as cl
-set total_dwdh_2012 = total_dwdh_2012.total_dwdh_2012
-from total_dwdh_2012
-where cl.tmc_code = total_dwdh_2012.tmc_code ;--and (date_part('hour', measurement_tstamp)  > 8 and date_part('hour', measurement_tstamp)  < 14);
-
-
-
-
-
-
+update truck_dwdh as cl
+set total_truck_dwdh_2012 = total_truck_dwdh_2012.total_truck_dwdh_2012
+from total_truck_dwdh_2012
+where cl.tmc_code = total_truck_dwdh_2012.tmc_code ;--and (date_part('hour', measurement_tstamp)  > 8 and date_part('hour', measurement_tstamp)  < 14);
--- truck_dwdh/total_truck_dwdh_2013.sql
+++ truck_dwdh/total_truck_dwdh_2013.sql
@@ -1,7 +1,7 @@
---drop table congestion_locations;
---create table congestion_locations as
-alter table congestion_locations
-add column if not exists total_dwdh_2013 numeric;
+--drop table truck_dwdh;
+--create table truck_dwdh as
+alter table truck_dwdh
+add column if not exists total_truck_dwdh_2013 numeric;
 
 with determine_delay_hours as(
 select i.*,
@@ -12,21 +12,21 @@ case when (i.tmc_code = t.tmc)
 o.aadt,
 
 case when (i.reference_speed*0.75 > i.speed)
-  THEN 1 
+  THEN (1/6)
   ELSE 0
 end as delay_hours,
 o.geom
 from tmacog_tmcs as o
 full outer join npmrds2012to2017data as i
-on o.tmc = i.tmc_code 
+on o.tmc = i.tmc_code
 full join tmc_identification as t on t.tmc = i.tmc_code
 --where i.cvalue > 10
 --where date_part('hour', measurement_tstamp)  =  --and date_part('hour', measurement_tstamp)  <= 14
 ),
-total_dwdh_2013 as
+total_truck_dwdh_2013 as
 (select
 tmc_code,
-round(sum(delay_hours*miles),2) as total_dwdh_2013
+round(sum(delay_hours*miles),2) as total_truck_dwdh_2013
 from determine_delay_hours
 where date_part('year', measurement_tstamp) = 2013
 group by tmc_code
@@ -34,12 +34,7 @@ group by tmc_code
 
 --for all hours
 
-update congestion_locations as cl
-set total_dwdh_2013 = total_dwdh_2013.total_dwdh_2013
-from total_dwdh_2013
-where cl.tmc_code = total_dwdh_2013.tmc_code ;--and (date_part('hour', measurement_tstamp)  > 8 and date_part('hour', measurement_tstamp)  < 14);
-
-
-
-
-
+update truck_dwdh as cl
+set total_truck_dwdh_2013 = total_truck_dwdh_2013.total_truck_dwdh_2013
+from total_truck_dwdh_2013
+where cl.tmc_code = total_truck_dwdh_2013.tmc_code ;--and (date_part('hour', measurement_tstamp)  > 8 and date_part('hour', measurement_tstamp)  < 14);
