90c3c6522c985b6a9625722d31f9397b9a2308ef
Mike Fuller <mrfuller460@gmail.com>
2018-08-23 16:54:12 -0400

    rename and update queries for total annual truck dwdh for 2012 to 2017

---
+++ truck_dwdh/total_truck_dwdh_2016.sql
@@ -0,0 +1,40 @@
+--drop table truck_dwdh;
+--create table truck_dwdh as
+alter table truck_dwdh
+add column if not exists total_truck_dwdh_2016 numeric;
+
+with determine_delay_hours as(
+select i.*,
+case when (i.tmc_code = t.tmc)
+	then t.miles
+	else o.miles
+	end as miles,
+o.aadt,
+
+case when (i.reference_speed*0.75 > i.speed)
+  THEN (1/6)
+  ELSE 0
+end as delay_hours,
+o.geom
+from tmacog_tmcs as o
+full outer join npmrds2012to2016truck10min_no_null as i
+on o.tmc = i.tmc_code
+full join tmc_identification as t on t.tmc = i.tmc_code
+--where i.cvalue > 10
+--where date_part('hour', measurement_tstamp)  =  --and date_part('hour', measurement_tstamp)  <= 14
+),
+total_truck_dwdh_2016 as
+(select
+tmc_code,
+round(sum(delay_hours*miles),2) as total_truck_dwdh_2016
+from determine_delay_hours
+where date_part('year', measurement_tstamp) = 2016
+group by tmc_code
+)
+
+--for all hours
+
+update truck_dwdh as cl
+set total_truck_dwdh_2016 = total_truck_dwdh_2016.total_truck_dwdh_2016
+from total_truck_dwdh_2016
+where cl.tmc_code = total_truck_dwdh_2016.tmc_code ;--and (date_part('hour', measurement_tstamp)  > 8 and date_part('hour', measurement_tstamp)  < 14);
---
+++ truck_dwdh/total_truck_dwdh_2017.sql
@@ -0,0 +1,41 @@
+--drop table truck_dwdh;
+--create table truck_dwdh as
+alter table truck_dwdh
+add column if not exists total_truck_dwdh_2017 numeric;
+
+with determine_delay_hours as(
+select i.*,
+case when (i.tmc_code = t.tmc)
+	then t.miles
+	else o.miles
+	end as miles,
+o.aadt,
+
+case when (i.reference_speed*0.75 > i.speed)
+  THEN (1/6)
+  ELSE 0
+end as delay_hours,
+o.geom
+from tmacog_tmcs as o
+full outer join npmrds2017truck10min_no_null as i
+on o.tmc = i.tmc_code
+full join tmc_identification as t on t.tmc = i.tmc_code
+--where i.cvalue > 10
+--where date_part('hour', measurement_tstamp)  =  --and date_part('hour', measurement_tstamp)  <= 14
+),
+total_truck_dwdh_2017 as
+(select
+tmc_code,
+round(sum(delay_hours*miles),2) as total_truck_dwdh_2017
+from determine_delay_hours
+where date_part('year',measurement_tstamp) = 2017
+--where date_part('hour', measurement_tstamp)  < 6 or date_part('hour', measurement_tstamp)  > 17
+group by tmc_code
+)
+
+--for all hours
+
+update truck_dwdh as cl
+set total_truck_dwdh_2017 = total_truck_dwdh_2017.total_truck_dwdh_2017
+from total_truck_dwdh_2017
+where cl.tmc_code = total_truck_dwdh_2017.tmc_code ;--and (date_part('hour', measurement_tstamp)  > 8 and date_part('hour', measurement_tstamp)  < 14);
