f2b353f1be6cbe11b6d1e867505eec8f530cb4b8
Mike Fuller <mrfuller460@gmail.com>
2018-09-06 12:15:46 -0400

    write query to find median tttr for each period

---
+++ tttr_medians.sql
@@ -0,0 +1,30 @@
+select percentile_disc(0.5) within group (order by tttr_amp_2017) as amp_2017_median,
+percentile_disc(0.5) within group (order by tttr_midd_2017) as midd__2017_median,
+percentile_disc(0.5) within group (order by tttr_pmp_2017) as pmp_2017_median,
+percentile_disc(0.5) within group (order by tttr_we_2017) as we_2017_median,
+
+percentile_disc(0.5) within group (order by tttr_amp_2016) as amp_2016_median,
+percentile_disc(0.5) within group (order by tttr_midd_2016) as midd__2016_median,
+percentile_disc(0.5) within group (order by tttr_pmp_2016) as pmp_2016_median,
+percentile_disc(0.5) within group (order by tttr_we_2016) as we_2016_median,
+
+percentile_disc(0.5) within group (order by tttr_amp_2015) as amp_2015_median,
+percentile_disc(0.5) within group (order by tttr_midd_2015) as midd__2015_median,
+percentile_disc(0.5) within group (order by tttr_pmp_2015) as pmp_2015_median,
+percentile_disc(0.5) within group (order by tttr_we_2015) as we_2015_median,
+
+percentile_disc(0.5) within group (order by tttr_amp_2014) as amp_2014_median,
+percentile_disc(0.5) within group (order by tttr_midd_2014) as midd__2014_median,
+percentile_disc(0.5) within group (order by tttr_pmp_2014) as pmp_2014_median,
+percentile_disc(0.5) within group (order by tttr_we_2014) as we_2014_median,
+
+percentile_disc(0.5) within group (order by tttr_amp_2013) as amp_2013_median,
+percentile_disc(0.5) within group (order by tttr_midd_2013) as midd__2013_median,
+percentile_disc(0.5) within group (order by tttr_pmp_2013) as pmp_2013_median,
+percentile_disc(0.5) within group (order by tttr_we_2013) as we_2013_median,
+
+percentile_disc(0.5) within group (order by tttr_amp_2012) as amp_2012_median,
+percentile_disc(0.5) within group (order by tttr_midd_2012) as midd__2012_median,
+percentile_disc(0.5) within group (order by tttr_pmp_2012) as pmp_2012_median,
+percentile_disc(0.5) within group (order by tttr_we_2012) as we_2012_median
+from tttr;
