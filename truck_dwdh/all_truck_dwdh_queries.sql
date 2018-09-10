ecf61c0ee39d4db04546a09d64c63510f09a08ac
Mike Fuller <mrfuller460@gmail.com>
2018-09-10 15:18:24 -0400

    create batch for all truck dwdh queries

---
+++ truck_dwdh/all_truck_dwdh_queries.sql
@@ -0,0 +1,46 @@
+--2017
+\i 'C:/*your_location*/npmrds/trucks/amp_truck_dwdh_2017.sql'
+\i 'C:/*your_location*/npmrds/trucks/midday_truck_dwdh_2017.sql'
+\i 'C:/*your_location*/npmrds/trucks/pmp_truck_dwdh_2017.sql'
+\i 'C:/*your_location*/npmrds/trucks/ovn_truck_dwdh_2017.sql'
+\i 'C:/*your_location*/npmrds/trucks/we_truck_dwdh_2017.sql'
+
+
+--2016
+\i 'C:/*your_location*/npmrds/trucks/amp_truck_dwdh_2016.sql'
+\i 'C:/*your_location*/npmrds/trucks/midday_truck_dwdh_2016.sql'
+\i 'C:/*your_location*/npmrds/trucks/pmp_truck_dwdh_2016.sql'
+\i 'C:/*your_location*/npmrds/trucks/ovn_truck_dwdh_2016.sql'
+\i 'C:/*your_location*/npmrds/trucks/we_truck_dwdh_2016.sql'
+
+
+--2015
+\i 'C:/*your_location*/npmrds/trucks/amp_truck_dwdh_2015.sql'
+\i 'C:/*your_location*/npmrds/trucks/midday_truck_dwdh_2015.sql'
+\i 'C:/*your_location*/npmrds/trucks/pmp_truck_dwdh_2015.sql'
+\i 'C:/*your_location*/npmrds/trucks/ovn_truck_dwdh_2015.sql'
+\i 'C:/*your_location*/npmrds/trucks/we_truck_dwdh_2015.sql'
+
+
+--2014
+\i 'C:/*your_location*/npmrds/trucks/amp_truck_dwdh_2014.sql'
+\i 'C:/*your_location*/npmrds/trucks/midday_truck_dwdh_2014.sql'
+\i 'C:/*your_location*/npmrds/trucks/pmp_truck_dwdh_2014.sql'
+\i 'C:/*your_location*/npmrds/trucks/ovn_truck_dwdh_2014.sql'
+\i 'C:/*your_location*/npmrds/trucks/we_truck_dwdh_2014.sql'
+
+
+--2013
+\i 'C:/*your_location*/npmrds/trucks/sql/amp_truck_dwdh_2013.sql'
+\i 'C:/*your_location*/npmrds/trucks/midday_truck_dwdh_2013.sql'
+\i 'C:/*your_location*/npmrds/trucks/pmp_truck_dwdh_2013.sql'
+\i 'C:/*your_location*/npmrds/trucks/ovn_truck_dwdh_2013.sql'
+\i 'C:/*your_location*/npmrds/trucks/we_truck_dwdh_2013.sql'
+
+
+--2012
+\i 'C:/*your_location*/npmrds/trucks/amp_truck_dwdh_2012.sql'
+\i 'C:/*your_location*/npmrds/trucks/midday_truck_dwdh_2012.sql'
+\i 'C:/*your_location*/npmrds/trucks/sql/pmp_truck_dwdh_2012.sql'
+\i 'C:/*your_location*/npmrds/trucks/sql/ovn_truck_dwdh_2012.sql'
+\i 'C:/*your_location*/npmrds/trucks/sql/we_truck_dwdh_2012.sql'
