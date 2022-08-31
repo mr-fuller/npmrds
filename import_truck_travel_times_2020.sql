87ccc27040388436c0da2431dc9a3912d1a897c3
mr-fuller <mrfuller460@gmail.com>
2022-08-31 15:21:20 -0400

    import scripts for FY23

---
+++ import_truck_travel_times_2020.sql
@@ -0,0 +1,13 @@
+drop table if exists lmw_2020_inrix_truck_seconds_nonull_10min;
+create table lmw_2020_inrix_truck_seconds_nonull_10min (
+tmc_code varchar(50),
+measurement_tstamp timestamp,
+speed numeric,
+average_speed numeric,
+reference_speed numeric,
+travel_time_seconds numeric,
+data_density varchar(9)
+);
+
+\copy lmw_2020_inrix_truck_seconds_nonull_10min from program 'unzip -p /home/fullerm/repos/npmrds/downloads/2020/truck/lmw_2020_inrix_truck_seconds_nonull_10min.zip lmw_2020_inrix_truck_seconds_nonull_10min.csv' with (format csv, header);
+
