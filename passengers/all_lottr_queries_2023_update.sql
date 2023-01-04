2ea88e5b0ee9b6f917307e77a0fc243ab191c354
mr-fuller <mrfuller460@gmail.com>
2023-01-04 10:55:36 -0500

    Create all_lottr_queries_2023_update.sql

---
+++ passengers/all_lottr_queries_2023_update.sql
@@ -0,0 +1,34 @@
+--2017
+\i '~/repos/npmrds/passengers/lottr_amp_2017.sql'
+\i '~/repos/npmrds/passengers/lottr_midd_2017.sql'
+\i '~/repos/npmrds/passengers/lottr_pmp_2017.sql'
+\i '~/repos/npmrds/passengers/lottr_we_2017.sql'
+--\i '~/repos/npmrds/passengers/total_lottr_2017.sql'
+
+--2018
+\i '~/repos/npmrds/passengers/lottr_amp_2018.sql'
+\i '~/repos/npmrds/passengers/lottr_midd_2018.sql'
+\i '~/repos/npmrds/passengers/lottr_pmp_2018.sql'
+\i '~/repos/npmrds/passengers/lottr_we_2018.sql'
+--\i '~/repos/npmrds/passengers/total_lottr_2018.sql'
+
+--2019
+\i '~/repos/npmrds/passengers/lottr_amp_2019.sql'
+\i '~/repos/npmrds/passengers/lottr_midd_2019.sql'
+\i '~/repos/npmrds/passengers/lottr_pmp_2019.sql'
+\i '~/repos/npmrds/passengers/lottr_we_2019.sql'
+--\i '~/repos/npmrds/passengers/total_lottr_2019.sql'
+
+--2020
+\i '~/repos/npmrds/passengers/lottr_amp_2020.sql'
+\i '~/repos/npmrds/passengers/lottr_midd_2020.sql'
+\i '~/repos/npmrds/passengers/lottr_pmp_2020.sql'
+\i '~/repos/npmrds/passengers/lottr_we_2020.sql'
+--\i '~/repos/npmrds/passengers/total_lottr_2020.sql'
+
+--2021
+\i '~/repos/npmrds/passengers/lottr_amp_2021.sql'
+\i '~/repos/npmrds/passengers/lottr_midd_2021.sql'
+\i '~/repos/npmrds/passengers/lottr_pmp_2021.sql'
+\i '~/repos/npmrds/passengers/lottr_we_2021.sql'
+--\i '~/repos/npmrds/passengers/total_lottr_2021.sql'
